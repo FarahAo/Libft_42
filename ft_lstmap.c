50bf42ae639bf55aa9c31aa4667be2ddc2e4c96f
FarahAo <farah.aboomar@gmail.com>
2026-09-17 07:23:28 +0300

    finish

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,7 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 {
-	t_list	new_node;
-
-
+	if (!lst)
+		return (NULL);
+	t_list	*new_node;
+	t_list	*new;
+	void	*content;
+	
+	new = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		new_node = ft_lstnew(content);
+		if (!new_node)
+		{
+			del(content);
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, new_node);
+		lst = lst->next;
+	}
+	return (new);
 }
